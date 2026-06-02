3c120a710a8a29ed11b52f914b9e732e68d25614
cks-wls <slo0521@naver.com>
2026-06-02 19:46:47 +0900

    feat : 공통 컴포넌트 EducationBox틀 구현(#1)

---
+++ src/components/common/EducationBox.tsx
@@ -0,0 +1,9 @@
+function EducationBox() {
+  return (
+    <>
+      <div className="border border-gray-300 rounded-xl">sdsd</div>
+    </>
+  );
+}
+// 이후에 데이터 받아와서 map으로 매핑하는 것 필요
+export default EducationBox;
