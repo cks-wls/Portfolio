bc88099a5f6e3fcba46bdd2b960ddbaff1fd4a90
cks-wls <slo0521@naver.com>
2026-06-03 20:19:04 +0900

    feat : 공통 컴포넌트 피그마 링크 이동 버튼 구현(#9)

---
+++ src/components/common/LinkToFigmaBtn.tsx
@@ -0,0 +1,20 @@
+import figmaBeforeHover from '@/assets/icons/figmaBeforeHover.svg';
+function LinkToFigmaBtn({ link }: { link: string }) {
+  const linkToFigma = ({ link }: { link: string }) => {
+    window.open(link, '_parent');
+  };
+  return (
+    <button
+      type="button"
+      className="p-[6px_12px] h-8 inline-flex items-center justify-center border-2 border-black rounded-full gap-2 cursor-pointer"
+      onClick={() => linkToFigma({ link })}
+    >
+      <img src={figmaBeforeHover} alt="FigmaLogo" />
+      <p className="hidden md:block md:text-md md:text-extrabold">
+        프로토타입 보러가기
+      </p>
+    </button>
+  );
+}
+// 추후에 hover했을때 바뀌는 부분 체크하기
+export default LinkToFigmaBtn;
